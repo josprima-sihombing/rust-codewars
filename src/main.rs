88645100a833c0c3661137137c1bb76c3294050c
Josprima Sihombing <jsihombing.id@gmail.com>
2024-10-26 00:01:58 +0700

    are you playing banjo

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,9 @@ fn main() {
     assert_eq!(solution::replace_dots("no dots"), "no dots");
     assert_eq!(solution::replace_dots("one.two.three"), "one-two-three");
     assert_eq!(solution::replace_dots("........"), "--------");
+
+    assert_eq!(solution::are_you_playing_banjo("Martin"), "Martin does not play banjo");
+    assert_eq!(solution::are_you_playing_banjo("Rikke"), "Rikke plays banjo");
+    assert_eq!(solution::are_you_playing_banjo("bravo"), "bravo does not play banjo");
+    assert_eq!(solution::are_you_playing_banjo("rolf"), "rolf plays banjo");
 }
--- src/solution.rs
+++ src/solution.rs
@@ -7,3 +7,13 @@ pub fn character_from_ascii(c: u8) -> char {
 pub fn replace_dots(s: &str) -> String {
   Regex::new(r"\.").unwrap().replace_all(s, "-").to_string()
 }
+
+pub fn are_you_playing_banjo(name: &str) -> String {
+  let first_char = name.chars().nth(0);
+
+  if first_char == Some('R') || first_char == Some('r') {
+    return String::from(name.to_owned() + " plays banjo");
+  }
+
+  String::from(name.to_owned() + " does not play banjo")
+}
